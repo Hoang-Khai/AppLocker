0226016d4c8d413a2311b82e91cafad63f00929c
PhamDat0 <datpqse05750@fpt.edu.vn>
2019-07-17 17:30:05 +0700

    Get app & icon

--- app/src/main/java/com/dkv/applocker/activity/SelectAppToLock.java
+++ app/src/main/java/com/dkv/applocker/activity/SelectAppToLock.java
@@ -14,5 +14,6 @@ public class SelectAppToLock extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_app_to_lock);
+
     }
 }
