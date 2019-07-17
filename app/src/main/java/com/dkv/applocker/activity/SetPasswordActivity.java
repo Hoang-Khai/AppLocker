1b8b7493d530f83cf68a6788aa98d61ba4dfcb4f
PhamDat0 <datpqse05750@fpt.edu.vn>
2019-07-17 11:18:13 +0700

    Add Unlock Activity
    Add Unlock Activity and Layout

--- app/src/main/java/com/dkv/applocker/activity/SetPasswordActivity.java
+++ app/src/main/java/com/dkv/applocker/activity/SetPasswordActivity.java
@@ -17,5 +17,7 @@ public class SetPasswordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_password);
+
+
     }
 }
