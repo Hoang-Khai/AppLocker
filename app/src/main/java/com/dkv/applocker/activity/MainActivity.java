79e2e1268b290f2c519cae7cde892908fccdbada
Fukami Yan <40638066+MinhDaoTanNguyetPha@users.noreply.github.com>
2019-07-18 06:19:45 +0700

    Nothing, just add some comment
    Nothing, just add some comment

--- app/src/main/java/com/dkv/applocker/activity/MainActivity.java
+++ app/src/main/java/com/dkv/applocker/activity/MainActivity.java
@@ -13,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //https://stackoverflow.com/questions/19852069/blocking-android-apps-programmatically/19852713#comment29526195_19852713
         //https://stackoverflow.com/questions/8061179/broadcast-receiver-to-detect-application-start
+        //https://github.com/savvisingh/AppLock
+        //https://github.com/SubhamTyagi/AppLock
+        //https://github.com/mattsilber/applock
     }
 }
