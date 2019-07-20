75329a2f45fe094ec1223434062d66e594ffa711
Fukami Yan <40638066+MinhDaoTanNguyetPha@users.noreply.github.com>
2019-07-20 15:51:19 +0700

    Revert "Merge branch 'master' of https://github.com/MinhDaoTanNguyetPha/AppLocker"
    This reverts commit 8378b125624c682bb7288f97be4b47ea5048909a, reversing
    changes made to 6fa3592d93c3bab38857f691218da650bda269de.

--- app/src/main/java/com/dkv/applocker/controller/AppListAdapter.java
+++ app/src/main/java/com/dkv/applocker/controller/AppListAdapter.java
@@ -1,6 +1,5 @@
 package com.dkv.applocker.controller;
 
-import android.app.Activity;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,51 +20,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AppListAdapter extends BaseAdapter {
-
-    private Activity aContext;
     private List<AppDisplayer> listApp;
     private LayoutInflater layoutInflater;
 
-    public AppListAdapter(Activity aContext, List<AppDisplayer> listApp){
-        this.aContext = aContext;
+    public AppListAdapter(Context aContext, List<AppDisplayer> listApp){
         this.listApp = listApp;
         layoutInflater = LayoutInflater.from(aContext);
     }
     @Override
     public int getCount() {
-        return listApp.size();
+        return 0;
     }
 
     @Override
     public Object getItem(int position) {
-        return listApp.get(position);
+        return null;
     }
 
     @Override
     public long getItemId(int position) {
-        return position;
+        return 0;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder holder;
-
-        //Tham chiếu đến layout
-
         if (convertView == null) {
-//            convertView = layoutInflater.inflate(R.layout.activity_app_list_adapter, null);
-            convertView = LayoutInflater.from(aContext).inflate(R.layout.activity_app_list_adapter,null);
+            convertView = layoutInflater.inflate(R.layout.activity_app_list_adapter, null);
             holder = new ViewHolder();
             holder.appIC = convertView.findViewById(R.id.imgAppIC);
             holder.appName = convertView.findViewById(R.id.txtAppName);
 //            holder.appStatus = (Switch) convertView.findViewById(R.id.btnStatus);
-
-            //gắn vào convertView
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder) convertView.getTag();
         }
-
         holder.appIC.setImageDrawable(listApp.get(position).getIcon());
         holder.appName.setText(listApp.get(position).getPackageName());
 //        holder.appStatus;
