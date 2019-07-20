8804d8e0a4229d1132e441e894e2be9c0718eef2
Fukami Yan <40638066+MinhDaoTanNguyetPha@users.noreply.github.com>
2019-07-20 15:44:25 +0700

    Finish services logic
    Add logic to let services lock application or unlock

--- app/src/main/java/com/dkv/applocker/helper_for_sql/SQLHelper.java
+++ app/src/main/java/com/dkv/applocker/helper_for_sql/SQLHelper.java
@@ -4,13 +4,14 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.dkv.applocker.model.LockedAppList;
 import com.dkv.applocker.model.Password;
 
 public class SQLHelper extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "AppLock.db";
     private static final String SQL_CREATE_PASSWORD_TABLE_ENTRIES = "CREATE TABLE " + Password.TABLE_NAME + " (" + Password.COLUMN_NAME_TOKEN +" TEXT)";
-    private static final String SQL_CREATE_APPLIST_TABLE_ENTRIES = "CREATE TABLE AppList (ID INTEGER PRIMARY KEY, AppName TEXT)";
+    private static final String SQL_CREATE_APPLIST_TABLE_ENTRIES = "CREATE TABLE " + LockedAppList.TABLE_NAME + " (" + LockedAppList.COLUMN_NAME_ID + " INTEGER PRIMARY KEY, "+ LockedAppList.COLUMN_NAME_APP_NAME + " TEXT)";
 
     public SQLHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
