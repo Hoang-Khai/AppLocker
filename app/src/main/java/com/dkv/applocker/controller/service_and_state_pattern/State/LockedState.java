13b2544e94c9d449af836edf02ea912e28d387ce
Fukami Yan <40638066+MinhDaoTanNguyetPha@users.noreply.github.com>
2019-07-20 15:54:18 +0700

    Revert "Merge branch 'master' of https://github.com/MinhDaoTanNguyetPha/AppLocker"
    This reverts commit b9270035c24d48ad1131df635a0d46b5c25cff5f, reversing
    changes made to 6e8d415718f0fd3b1fe6e5b607ae8efd0cbf7d89.

--- app/src/main/java/com/dkv/applocker/controller/service_and_state_pattern/State/LockedState.java
+++ app/src/main/java/com/dkv/applocker/controller/service_and_state_pattern/State/LockedState.java
@@ -1,32 +1,15 @@
 package com.dkv.applocker.controller.service_and_state_pattern.State;
 
-import android.content.Context;
-import android.content.Intent;
-
-import com.dkv.applocker.activity.UnlockActivity;
-import com.dkv.applocker.controller.service_and_state_pattern.TopActivityProcessService;
-import com.dkv.applocker.model.LockedAppList;
+import com.dkv.applocker.controller.service_and_state_pattern.State.State;
 
 public class LockedState implements State {
+    @Override
+    public void enterPassword() {
 
-    TopActivityProcessService parent;
-
-    public LockedState(TopActivityProcessService topActivityProcessService) {
-        parent = topActivityProcessService;
     }
 
     @Override
-    public void changeActivity(String forePackage, Context context) {
-        LockedAppList list = new LockedAppList(context);
-        if(list.hasForePackageLocked(forePackage)) {
-            askUserToEnterPassword(context);
-        }
+    public void changeActivity() {
+
     }
-
-    private void askUserToEnterPassword(Context context) {
-        Intent intent = new Intent(context, UnlockActivity.class);
-        context.startActivity(intent);
-    }
-
-
 }
--- app/src/main/java/com/dkv/applocker/controller/service_and_state_pattern/State/State.java
+++ app/src/main/java/com/dkv/applocker/controller/service_and_state_pattern/State/State.java
@@ -1,7 +1,6 @@
 package com.dkv.applocker.controller.service_and_state_pattern.State;
 
-import android.content.Context;
-
 public interface State {
-    public void changeActivity(String forePackage, Context context);
+    public void enterPassword();
+    public void changeActivity();
 }
